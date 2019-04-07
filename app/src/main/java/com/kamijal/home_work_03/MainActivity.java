ac608101af6bf0b66b3cbde014edecb47ec84bf4
Kamil <ushurbakiev.k@gmail.com>
2019-04-07 06:32:09 +0600

    Initial commit

---
+++ app/src/main/java/com/kamijal/home_work_03/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kamijal.home_work_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
