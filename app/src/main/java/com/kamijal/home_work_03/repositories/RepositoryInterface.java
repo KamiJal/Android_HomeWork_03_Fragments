b398d7b5374debe85eabe1e6797de9ca16153b02
Kamil <ushurbakiev.k@gmail.com>
2019-04-09 19:08:36 +0600

    added_repositories.RepositoryInterface

---
+++ app/src/main/java/com/kamijal/home_work_03/repositories/RepositoryInterface.java
@@ -0,0 +1,9 @@
+package com.kamijal.home_work_03.repositories;
+
+import com.kamijal.home_work_03.models.Video;
+
+import java.util.List;
+
+public interface RepositoryInterface {
+    List<Video> getVideoList();
+}
